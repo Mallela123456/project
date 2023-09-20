938578e67143a9d7c65d43c3d6b62872e991c226
srikanth <sri@123>
2023-09-20 15:53:59 +0530

    mess

---
+++ pro.py/pro.py
@@ -0,0 +1,72 @@
+#Write a python program to remove a given  character from string.
+#name = "srikanth"
+#remove_1 = name.replace("s", " ")
+#print(remove_1)
+
+#Write a program to check String is Palindrome or not.
+#string = input("Enter string: ")
+#palindrome = string[::-1]
+#if palindrome == string:
+#   print("this is palindrome")
+#else:
+ #   print("this is not palindrome")
+ 
+#Write a python program to check given character is vowel or consonant.
+#char ="U"
+#if char == "a" or char =="e" or char == "i" or char == "o" or char == "u":
+ #   print(char, "this vowel")
+#elif char == "A" or char =="E" or char == "I" or char == "O" or char == "U":
+ #   print(char, "this is consonent")
+#else:
+ #print("not found")
+ 
+#Write a python program to replace string space with given character in Python.
+#string = "Srikanth mallela"
+#string_replace = string.replace(" ","$")
+#print(string_replace)
+
+#Write a python program to count alphabets, digits, and special characters in the string.
+#string = input("Enter string: ")
+#alphabets = 0 
+#digits = 0 
+#special = 0 
+#for  i in range(len(string)):
+    
+ #   if (string[i].isalpha()):
+  #      alphabets = alphabets + 1 
+   # elif (string[i].isdigit()):
+    #    digits = digits + 1 
+    #else:
+     #   special = special + 1
+     
+#6.Write a python program to remove all the blank spaces in a given string.
+#name = ",,,,,,,,,,,srikanth..........."
+#print(name.strip(",."))
+
+#7.Write a python program to find sum of integers in the string.
+
+#a = input("Enter the string: ") 
+#sum = 0
+#for i in a:
+ #   if i.isdigit():
+  #      sum = sum + int(i)
+#print(sum)
+
+#8.Write a python program to Remove Repeated Character from String.
+#string = "sssssrikanthhhh"
+#k2 = " "
+#for char in string:
+# if char not in k2:
+     #k2=k2+char
+#print(k2)
+
+#9.Write a python program to count occurrence of given character in string.
+
+#string = "srikanthmallela"
+#count = 0
+#for i in string:
+ #   if i == "a":
+  #      count = count + 1
+#print(count)
+
+#10.Write a python program to check string is anagrams or not in Python.
